d9cd8fa5398ec7a03f9aae009ffdadcdb65b7f06
markiine <dmkotkas@gmail.com>
2022-09-08 16:48:39 +0300

    Update Program.cs

--- ParcelSorting/Program.cs
+++ ParcelSorting/Program.cs
@@ -7,7 +7,7 @@ namespace ParcelSorting
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! Wasssaaa!");
 
             ParcelLineFitController(BoxSizes);
         }
