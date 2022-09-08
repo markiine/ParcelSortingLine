a9057a2ce4389f98b5239317a66d3938ae404cf1
markiine <dmkotkas@gmail.com>
2022-09-08 16:42:28 +0300

    testimine

---
+++ BoxAndSortingLine.Test/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using ParcelSorting;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace BoxAndSortingLine.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void When_ParcelHasNewDimensionsAndSortingLineHasNewDimensions_Then_ParcelCanNotFitSortingLine()
+        {
+            var boxSizes = new List<BoxSize>()
+            {
+                new BoxSize
+                {
+                    Length = 70,
+                    Width = 50,
+                    SortingLineParams = new List<SortingLineParam>()
+                    {
+                        new SortingLineParam
+                        {
+                            LineWidth = 50,
+                        },
+                        new SortingLineParam
+                        {
+                            LineWidth = 50,
+                        },
+                        new SortingLineParam
+                        {
+                            LineWidth = 100,
+                        },
+                        new SortingLineParam
+                        {
+                            LineWidth = 100,
+                        }
+                    }
+                }
+            };
+
+            bool result = Program.ParcelLineFitController(boxSizes);
+
+            Assert.True(result);
+        }
+    }
+}
